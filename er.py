84e8ab9dd743f5e597fa4fc98669cd6c37be39af
am5egor <156437844+am5egor@users.noreply.github.com>
2024-07-28 21:33:47 +0300

    Add files via upload

---
+++ er.py
@@ -0,0 +1,17 @@
+import pygetwindow
+import psutil
+import time
+
+while True:
+    
+    windows = pygetwindow.getAllTitles()
+    
+    for window in windows:
+        if 'Браузер' in window and 'YouTube' in window:
+            
+            procs = psutil.process_iter(['pid', 'name'])
+            for proc in procs:
+                if proc.info['name'] == 'browser.exe':
+                    proc.kill()
+    
+    time.sleep(1)
